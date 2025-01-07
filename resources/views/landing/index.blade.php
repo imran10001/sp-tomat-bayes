d2a43eb429e35c792fa58396a34db489e1f26797
imran10001 <imransupu01@gmail.com>
2025-01-07 16:01:22 +0900

    perbaikan layout list riwayat

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -83,7 +83,7 @@
   <main>
     <div class="container">
       <div class="row">
-        <section class="  gap-3 d-flex align-items-center" id="beranda">
+        <section class="  gap-3 d-flex align-items-center" id="beranda" style="min-height: 100vh">
           <img src="{{asset('landing/asset/icons/tomato-hero.png')}}" alt="TOMAT" class="w-50" />
           <div class="banner">
             <h1 class="fw-bolder">SISTEM PAKAR DIAGNOSA PENYAKIT TANAMAN TOMAT</h1>
@@ -142,18 +142,21 @@
             {{-- <tbody class="pt-2"> --}}
               @foreach ($diagnosis_data as $item)
               <div class="card border-0 mb-3" style="width: 100%;">
-                <div class="row g-0 d-flex align-items-center justify-content-center">
-                  <div class="col-md-2 d-flex justify-content-center align-items-center border border-1 rounded-5" style=" width: 150px; height: 150px; overflow:hidden;">
-                    <img src="storage/Hypothesis-Image/{{$item->hypothesis->images->first()->image_path}}" class="rounded-start border" width="100%"  alt="...">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title fw-bolder">{{$item->hypothesis->name}}</h5>
-                      <p class="card-text">{{$item->value}}%</p>
-                      <p class="card-text">{{$item->created_at->format('d/m/Y')}}</p>
+                <div class=" g-0 d-flex flex-row align-items-center justify-content-between mx-5">
+                  <div class="d-flex">
+                    <div class="col-md-2 d-flex justify-content-center align-items-center border border-1 rounded-5" style=" width: 150px; height: 150px; overflow:hidden;">
+                      <img src="storage/Hypothesis-Image/{{$item->hypothesis->images->first()->image_path}}" class="rounded-start border" width="100%"  alt="...">
+                    </div>
+                    <div class="col ">
+                      <div class="card-body">
+                        <h5 class="card-title fw-bolder">{{$item->hypothesis->name}}</h5>
+                        <p class="card-text">{{$item->value}}%</p>
+                        <p class="card-text">{{$item->created_at->format('d/m/Y')}}</p>
+                      </div>                    
                     </div>
                   </div>
-                  <div class="col-md-2 d-flex justify-content-end gap-1">
+
+                  <div class="col-md-1 d-flex justify-content-center justify-content-md-end gap-1 ">
                     <button type="button" class="btn btn-danger text-light" data-bs-toggle="modal" data-bs-target="#modalId{{ $item->id }}"><i class="fa-solid fa-trash"></i></button>
                     <a href="{{ route('history_detail',  $item->id)}}" class="btn btn-primary text-light"><i class="fa-solid fa-chevron-right"></i></a>
                   </div>
